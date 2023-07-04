f00322f09deaa39effc127af67e0893c684cbcf2
Dilshod <dilshodsh97@gmail.com>
2023-07-04 12:46:38 +0500

    Rabbit MQ Exchange types added

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,8 +7,13 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-          //  Publisher.SendMessage();
-            Subscriber.GetMessage();
+            while (true)
+            {
+                Publisher.SendMessage();
+                Console.WriteLine();
+
+            }
+          //  Subscriber.GetMessage();
             //List<int> numbers = new() { 3, 5, 12, 15, 17, 22, 46, 97, 125, 234, 456, 567, 789, 1234 };
 
             //Console.WriteLine("Index:" + numbers.BinarySearching(567));
--- Algorithms/RabbitMq/Publisher.cs
+++ Algorithms/RabbitMq/Publisher.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using System.ComponentModel;
 using System.Text;
 
 namespace Algorithms.RabbitMq
@@ -7,16 +8,30 @@ namespace Algorithms.RabbitMq
     {
         public static void SendMessage()
         {
-            var factory = new ConnectionFactory { HostName = "localhost" };
+            var factory = new ConnectionFactory
+            {
+                HostName = "178.218.201.17",
+                UserName = "jamshid",
+                Password = "12345"
+            };
+
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
             
+            //var prop= channel.CreateBasicProperties();
+            
+            //Dictionary<string, object> properties = new();
+
+            //properties.Add("Content-Type", "text");
+            //properties.Add("Token", "123");
+            //properties.Add("Username", "Jamshid");
+
+         //   prop.Headers = properties;
             const string message = "Salom Hello World!";
 
             var body = Encoding.UTF8.GetBytes(message);
-
-            channel.BasicPublish(exchange: "Custom",
-                                 routingKey: "RouteKey",
+            channel.BasicPublish(exchange: "InfoLog",
+                                 routingKey: "InfoKey",
                                  basicProperties: null,
                                  body: body);
             Console.WriteLine($" [x] Sent {message}");
